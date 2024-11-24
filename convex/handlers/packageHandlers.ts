3f322dd858691f5535192cdf868fd6c2164b8c5a
Dhoka, Nikhil <ndhoka@iu.edu>
2024-11-24 14:26:39 -0500

    Pagination Working on /packages endpoint

--- convex/handlers/packageHandlers.ts
+++ convex/handlers/packageHandlers.ts
@@ -2,6 +2,18 @@ import { api } from "../_generated/api";
 import { ActionCtx, httpAction } from "../_generated/server";
 
 export const getPackagesHTTPHandler = httpAction(async (ctx, request) => {
-  const packages = await ctx.runQuery(api.queries.packageTable.getPackagesMetadata, {});
-  return new Response(JSON.stringify(packages), { status: 200 });
+  // get the offset parameter from the request and then use that to paginate the response. In the response, also return the next offset in the response headers
+  const url = new URL(request.url);
+  const offset = url.searchParams.get("offset") || null;
+  console.log('offset:', offset);
+  const limit = 2;
+  const result = await ctx.runQuery(api.queries.packageTable.getPackagesMetadata, {paginationOpts: {numItems: limit, cursor: offset }, });
+  const nextOffset = result.cursor; // Cursor for the next page
+  console.log('nextOffset:', nextOffset);
+  const headers = new Headers();
+  if (nextOffset) {
+    headers.set("offset", nextOffset);
+  }
+  console.log("Headers:", headers);
+  return new Response(JSON.stringify(result.packagesData), { status: 200, headers });
 });
--- convex/queries/packageTable.ts
+++ convex/queries/packageTable.ts
@@ -1,5 +1,6 @@
 import { query } from "../_generated/server";
 import { v } from "convex/values";
+import { paginationOptsValidator } from "convex/server";
 
 export const getPackageById = query({
   args: { packageId: v.id("packageTable") }, // Validate that packageId is an ID from "packageTable"
@@ -15,16 +16,22 @@ export const getPackageById = query({
 
 
 export const getPackagesMetadata = query({
-    args: {},
+    args: {paginationOpts: paginationOptsValidator},
     handler: async (ctx: any, args: any) => {
-         
-        const pkgs = await ctx.db.query("packageTable").collect(); // Fetch all packages
+         console.log('args:', args);
+        const result = await ctx.db.query("packageTable").paginate(args.paginationOpts); // Fetch all packages
+        console.log('Paginated Query Result:', result);
+        const pkgs = result.page;
         pkgs.forEach((pkg: any) => {
             pkg.metadata.ID = pkg._id;
         });
         //only return the metadata
         const packagesMetadata = pkgs.map((pkg: any) => pkg.metadata);
-        return packagesMetadata;
+        console.log('Packages Metadata:', packagesMetadata);
+        return {
+            packagesData: packagesMetadata,
+            cursor: result. continueCursor,
+        };
     },
 });
   
