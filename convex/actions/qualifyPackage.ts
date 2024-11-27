3bbe83d9c403ebb129bd931fa1dbc404c52c26e8
fuga <cg3122492@gmail.com>
2024-11-26 23:29:15 -0500

    working on getting version info for link and content

--- convex/actions/qualifyPackage.ts
+++ convex/actions/qualifyPackage.ts
@@ -18,6 +18,7 @@ export const qualifyPackage = action({
 		Content: v.optional(v.string()),
 		Name: v.optional(v.string()),
 		URL: v.optional(v.string()),	
+		Debloat: v.optional(v.boolean()),
 	},
 	handler: async (ctx: ActionCtx, args) => {
 		if ('Content' in args && 'URL' in args) {
@@ -28,9 +29,18 @@ export const qualifyPackage = action({
 				},
 			};
 		} else if ('Content' in args) { // proceed with 2-5.
-			const { Content, Name } = args;
-			const Version = "1.0.0"; // TODO: Make a search method that checks only for name since I can't get version from content. 
+			const { Content, Name, Debloat } = args;
+			const Version = "1.0.0"; // TODO: Iterate through files, find package.json. Get version info from there as well as name.
 			
+			if (!Content) {
+				return {
+					conflict: true,
+					metadata: {
+						message: "Content must not be empty.",
+					}
+				};
+			}
+
 			if (!Name) {
 				return {
 					conflict: true,
@@ -57,13 +67,21 @@ export const qualifyPackage = action({
 			}
 
 			// Calculate package scores. 
+			
+
 			// Decide if data should be written.
+
 			// Debloat if flagged.
+			let processedContent = Content;
+			if (Debloat === true) {
+				processedContent = await debloatBase64Package(Content);
+			}
+
 			// Store data by calling mutation.
 			let packageID: String = await ctx.runMutation(api.mutations.uploadPackage.uploadPackage, {
 				packageName: Name,
 				packageVersion: Version,
-				Content: Content,
+				Content: processedContent,
 			}); // return the uniqueID to package with all the other details.
 
 			return {
@@ -72,14 +90,14 @@ export const qualifyPackage = action({
 					packageName: Name,
 					packageVersion: Version,
 					packageID,
-					Content: Content,
+					Content: processedContent,
 				}
 			};
 
 			
 		} else if ('URL' in args) { // proceed with 2-5.
 			const { URL } = args;
-			const Version = "1.0.0"; // TODO: Get package version via link.
+			const Version = "1.0.0"; // TODO: Can get version from package.json file. How to download content from GitHub?
 
 			if (!URL) {
 				return {
